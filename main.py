d6a1b663c61a87cb02a1a59e4095561a2e2dd7f3
RyanAngWeiHao <98933340+RyanAngWeiHao@users.noreply.github.com>
2022-07-02 23:21:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])        # Allow the REST API endpoint to take in GET/POST request(s)
 def RESTFUL():
-    if request.method == 'GET':
+    if request.method == 'GET':                 # [IMPORTANT] By default, when the client first enters the URL, he is in fact sending a GET request
         return render_template('index.html')
 
-    elif request.method == 'POST':
-        response = request.post(url="http://backend-service:5010/backend")
-        return render_template('index.html', content=response.text)
+    elif request.method == 'POST':              # [IMPORTANT] A POST request will be triggered/sent to the rest API whenever a FORM (via html) is submitted
+        response = request.post(url="http://backend-service:5010/")      # [SUPER IMPORTANT] This sends the request to ANOTHER REST API to carry out the processing
+        return render_template('index.html', content=response.text)             # "response.text"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
