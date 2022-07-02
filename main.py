0c409082ba5e9d77df9cc66917430bad3aa9dc74
RyanAngWeiHao <98933340+RyanAngWeiHao@users.noreply.github.com>
2022-07-02 16:12:44 +0700

    Base Code

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def RESTFUL():
+    if request.method == 'GET':
+        return render_template('index.html')
+
+    elif request.method == 'POST':
+        text = request.form['texts']     # request.form converts data to python dictionary already
+        response = request.post(url="http://nameOfTheService:svcport/...")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
