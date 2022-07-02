dcd06ab79eaddd2ac3a69fc93036d285b2bfa0ff
RyanAngWeiHao <98933340+RyanAngWeiHao@users.noreply.github.com>
2022-07-03 01:05:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def RESTFUL():
 
     elif request.method == 'POST':              # [IMPORTANT] A POST request will be triggered/sent to the rest API whenever a FORM (via html) is submitted
         response = requests.post(url="http://backend-service:5010/")      # [SUPER IMPORTANT] This sends the request to ANOTHER REST API to carry out the processing
-        return render_template('index.html', content=response)       # "response.text" extracts the text data from the REST API RESPONSE
+        return render_template('index.html', content=response.text)       # "response.text" extracts the text data from the REST API RESPONSE
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
