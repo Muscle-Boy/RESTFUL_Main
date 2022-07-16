6ec0182b7e1ea07272febdb59d5328cd68b9f4e7
Ryan A <98933340+Muscle-Boy@users.noreply.github.com>
2022-07-17 01:34:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ def RESTFUL():
         
         # Inserting Data into MongoDB Database via its collection
         record = [{
-            "Type":"Text"
-            "text":response.text                      # "response.text" extracts the text data from the REST API RESPONSE
+            "Type": "Text"
+            "Text": str(response.text)                      # "response.text" extracts the text data from the REST API RESPONSE
         }]        
         information.insert_many(record)
         
